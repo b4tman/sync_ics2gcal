f975ddce67d5ca73928685c8d2b61684978d5b32
Dmitry Belyaev <b4tm4n@mail.ru>
2020-02-19 12:37:49 +0300

    add files for setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sync-ics2gcal',
+    author='Dmitry Belyaev',
+    author_email='b4tm4n@mail.ru',
+    license='MIT',
+    description='Sync ics file with Google calendar',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/b4tman/sync_ics2gcal',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.5',
+    install_requires = [
+        'google-auth>=1.5.0',
+        'google-api-python-client>=1.7.0',
+        'icalendar>=4.0.1',
+        'pytz',
+        'PyYAML>=3.13'
+    ],
+    scripts=['manage-calendars.py', 'sync-calendar.py']
+)
