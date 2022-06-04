e5abc3c21826442c4d63b9a9cd874ab2f0b562e8
Dmitry <b4tm4n@mail.ru>
2022-06-04 17:12:21 +0300

    fix test error

--- sync_ics2gcal/sync_calendar.py
+++ sync_ics2gcal/sync_calendar.py
@@ -27,7 +27,7 @@ def get_start_date(date: ConfigDate) -> datetime.datetime:
     return result
 
 
-def main() -> str:
+def main() -> None:
     config = load_config()
 
     if "logging" in config:
